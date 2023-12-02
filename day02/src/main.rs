445c0e58e369c58e490a79400395613f0f6cb9a9
Topias Heinonen <topias.heinonen@gofore.com>
2023-12-02 11:26:08 +0200

    Day 2 comined parsing for parts 1 and 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -4,24 +4,20 @@ use regex::Regex;
 fn main() {
     aoc::init_logging();
 
-    let lines = aoc::read_input_lines();
-
-    part1(&lines);
-
-    part2(&lines);
-}
-
-fn part1(lines: &[String]) {
-    let maxes = lines
-        .iter()
-        .map(|line| parse_line(line))
+    let maxes = aoc::input_lines()
+        .map(|line| parse_line(&line))
         .collect::<Vec<[u32; 3]>>();
     debug! {"Maxes: {maxes:?}"};
 
+    part1(&maxes);
+    part2(&maxes);
+}
+
+fn part1(maxes: &[[u32; 3]]) {
     let sum: usize = maxes
         .iter()
         .enumerate()
-        .filter(|(_, [r, g, b])| *r < 13 && *g < 14 && *b < 15)
+        .filter(|(_, [r, g, b])| *r <= 12 && *g <= 13 && *b <= 14)
         .map(|(i, _)| i + 1)
         .sum();
 
@@ -48,12 +44,7 @@ fn parse_line(line: &str) -> [u32; 3] {
     [max_red, max_green, max_blue]
 }
 
-fn part2(lines: &[String]) {
-    let maxes = lines
-        .iter()
-        .map(|line| parse_line(line))
-        .collect::<Vec<[u32; 3]>>();
-    debug! {"Maxes: {maxes:?}"};
+fn part2(maxes: &[[u32; 3]]) {
     let sum: u32 = maxes.iter().map(|[r, g, b]| r * g * b).sum();
 
     info!("Part 2: {sum}");
