359965333ba5c41d864b4d19ddaa42c6ce84a049
Topias Heinonen <topias.heinonen@gofore.com>
2023-12-08 09:16:31 +0200

    Day 8 cleaned though not pretty

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,29 +1,36 @@
 use std::collections::HashMap;
 
-use log::{debug, info};
+use log::debug;
 use num::Integer;
 
 const INPUT: &str = include_str!("../input.txt");
-const EXAMPLE: &str = include_str!("../example.txt");
+const EXAMPLE2: &str = include_str!("../example2.txt");
 
 fn main() {
-    aoc::run_with_bench(INPUT, EXAMPLE, &|aoc| {
+    aoc::run_with_bench(INPUT, EXAMPLE2, &|aoc| {
         let lines = aoc.read_input_lines();
 
-        (0, part2(&lines))
+        let (directions, nodes) = parse(&lines);
+        debug!("{directions:?}");
+        debug!("{nodes:?}");
+
+        (part1(directions, &nodes), part2(directions, &nodes))
     });
 }
 
-fn part1(lines: &[&str]) -> u32 {
+fn parse<'a>(lines: &'a [&str]) -> (&'a str, HashMap<&'a str, (&'a str, &'a str)>) {
     let directions = lines[0];
-    debug!("{directions:?}");
-    let mut dirs = directions.chars().cycle();
+
     let nodes = lines[2..]
         .iter()
         .map(|line| (&line[0..=2], (&line[7..=9], &line[12..=14])))
         .collect::<HashMap<_, _>>();
-    debug!("{nodes:?}");
 
+    (directions, nodes)
+}
+
+fn part1(directions: &str, nodes: &HashMap<&str, (&str, &str)>) -> u32 {
+    let mut dirs = directions.chars().cycle();
     let mut steps: u32 = 0;
     let mut current = "AAA";
     while current != "ZZZ" {
@@ -34,25 +41,14 @@ fn part1(lines: &[&str]) -> u32 {
         } else {
             current = nodes[current].1
         }
-
         steps += 1;
     }
 
-    return steps;
+    steps
 }
 
-fn part2(lines: &[&str]) -> u64 {
-    let directions = lines[0];
-    debug!("{directions:?}");
-    let mut dirs = directions.chars().cycle();
-    let nodes = lines[2..]
-        .iter()
-        .map(|line| (&line[0..=2], (&line[7..=9], &line[12..=14])))
-        .collect::<HashMap<_, _>>();
-    debug!("{nodes:?}");
-
-    let mut steps: u64 = 0;
-    let mut current = nodes
+fn part2(directions: &str, nodes: &HashMap<&str, (&str, &str)>) -> u64 {
+    let current = nodes
         .keys()
         .filter(|key| key.ends_with('A'))
         .map(|a| a.to_owned())
@@ -67,68 +63,53 @@ fn part2(lines: &[&str]) -> u64 {
             let mut steps: u64 = 0;
             while !c.ends_with('Z') {
                 let next_dir = dirs.next();
-                debug!("{c} {next_dir:?}");
                 if let Some('L') = next_dir {
                     c = nodes[c].0
                 } else {
                     c = nodes[c].1
                 }
-
                 steps += 1;
             }
             steps
         })
-        .inspect(|c| info!("{c}"))
+        .inspect(|c| debug!("{c}"))
         .collect::<Vec<u64>>();
 
-    info!("{cycles:?}");
+    debug!("{cycles:?}");
 
-    let lcm = cycles.into_iter().reduce(|a, b| a.lcm(&b)).unwrap();
-    /*
-    while !current.iter().all(|c| c.ends_with('Z')) {
-                let next_dir = dirs.next();
-        if steps % 1000000 == 0 {debug!("{steps} {current:?} {next_dir:?}")}
-        //debug!("{current:?} {next_dir:?}");
-        if let Some('L') = next_dir {
-            current = current.iter().map(|&c| nodes[c].0).collect::<Vec<_>>();
-        } else {
-            current = current.iter().map(|&c| nodes[c].1).collect::<Vec<_>>();
-
-        }
-
-        steps +=1;
-    }
-
-
-    return steps
-     */
-    lcm
+    cycles.into_iter().reduce(|a, b| a.lcm(&b)).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
+    const EXAMPLE1: &str = include_str!("../example1.txt");
+
     use super::*;
     #[test]
     fn part1_with_example() {
-        let lines = EXAMPLE.lines().collect::<Vec<_>>();
-        assert_eq!(part1(&lines), 0);
+        let lines = EXAMPLE1.lines().collect::<Vec<_>>();
+        let (dir, nodes) = parse(&lines);
+        assert_eq!(part1(dir, &nodes), 6);
     }
 
     #[test]
     fn part1_with_input() {
         let lines = INPUT.lines().collect::<Vec<_>>();
-        assert_eq!(part1(&lines), 0);
+        let (dir, nodes) = parse(&lines);
+        assert_eq!(part1(dir, &nodes), 20221);
     }
 
     #[test]
     fn part2_with_example() {
-        let lines = EXAMPLE.lines().collect::<Vec<_>>();
-        assert_eq!(part2(&lines), 0);
+        let lines = EXAMPLE2.lines().collect::<Vec<_>>();
+        let (dir, nodes) = parse(&lines);
+        assert_eq!(part2(dir, &nodes), 6);
     }
 
     #[test]
     fn part2_with_input() {
         let lines = INPUT.lines().collect::<Vec<_>>();
-        assert_eq!(part2(&lines), 0);
+        let (dir, nodes) = parse(&lines);
+        assert_eq!(part2(dir, &nodes), 14616363770447);
     }
 }
