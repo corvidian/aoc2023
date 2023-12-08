b66588866538a0b0ae48af97dc7c025d30d10077
Topias Heinonen <topias.heinonen@gofore.com>
2023-12-08 08:20:26 +0200

    Day 8 Solved

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,4 @@
+use aoc::parse_numbers;
 use log::debug;
 
 const INPUT: &str = include_str!("../input.txt");
@@ -7,11 +8,11 @@ fn main() {
     aoc::run_with_bench(INPUT, EXAMPLE, &|aoc| {
         let lines = aoc.read_input_lines();
         let (seeds, groups) = parse_input(lines);
-        (part1(&seeds, &groups), part2(&seeds, &groups))
+        (0, part2(&seeds, &groups))
     });
 }
 
-fn part1(seeds: &[u32], groups: &[Vec<Vec<u32>>]) -> u32 {
+fn _part1(seeds: &[u32], groups: &[Vec<Vec<u32>>]) -> u32 {
     seeds
         .iter()
         .map(|seed| map_seed(*seed, groups))
@@ -52,18 +53,12 @@ fn map_seed_in_group(seed: u32, group: &[Vec<u32>]) -> u32 {
         let source = rule[1];
         let range = rule[2];
         if source <= seed && seed <= source + range {
-            //debug!("Seed {seed} maps to: {}", destination + seed - source);
+            debug!("Seed {seed} maps to: {}", destination + seed - source);
             return destination + seed - source;
         }
     }
-    //debug!("Seed stays: {seed}");
+    debug!("Seed stays: {seed}");
     seed
-}
-
-fn parse_numbers(list: &str) -> Vec<u32> {
-    list.split_whitespace()
-        .map(|n| n.parse::<u32>().expect("Not a number!"))
-        .collect::<Vec<_>>()
 }
 
 fn part2(seeds: &[u32], groups: &[Vec<Vec<u32>>]) -> u32 {
@@ -71,7 +66,9 @@ fn part2(seeds: &[u32], groups: &[Vec<Vec<u32>>]) -> u32 {
     seeds.chunks_exact(2).for_each(|pair| {
         debug!("for seeds in {:?}", pair);
         for seed in pair[0]..pair[0] + pair[1] {
-            let mapped = map_seed(seed, &groups);
+            let mapped = map_seed(seed, groups);
+            debug!("Destination: {mapped}");
+
             if mapped < min {
                 debug!("New min: {mapped} replaces old one {min}");
                 min = mapped
@@ -79,4 +76,77 @@ fn part2(seeds: &[u32], groups: &[Vec<Vec<u32>>]) -> u32 {
         }
     });
     min
+}
+
+pub fn map_seed_range_in_group(seeds: (u32, u32), group: &[Vec<u32>]) -> Vec<(u32, u32)> {
+    let (seed_start, seed_end) = seeds;
+    for rule in group {
+        let destination = rule[0];
+        let source = rule[1];
+        let range = rule[2];
+
+        if source <= seed_start && source + range >= seed_end {
+            println!("Whole seed range of ({seed_start},{seed_end}) is inside rule ({source}, {range}), so moving seeds to ({},{})", destination + seed_start - source, destination + seed_end - source);
+            return vec![(
+                destination + seed_start - source,
+                destination + seed_end - source,
+            )];
+        }
+        if source >= seed_start && source + range <= seed_end {
+            println!("Middle of seed range ({seed_start},{seed_end}) is completely inside rule ({source}, {range}), splitting to three");
+
+            return vec![(
+                destination + seed_start - source,
+                destination + seed_end - source,
+            )];
+        }
+    }
+    println!(
+        "Seeds ({seed_start},{seed_end}) didn't match any rules in {:?}",
+        group
+    );
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn one_step_split_whole_range_inside_rule() {
+        let seeds = (79u32, 93);
+        let group = vec![vec![50u32, 98, 2], vec![52, 50, 48]];
+
+        let result = map_seed_range_in_group(seeds, &group);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0], (81, 95));
+    }
+
+    #[test]
+    fn map_seeds_with_middle_part_inside_rule() {
+        let seeds = 55..68;
+        let group = vec![vec![50u32, 60, 2]];
+
+        let result = seeds
+            .map(|seed| map_seed_in_group(seed, &group))
+            .collect::<Vec<_>>();
+
+        assert_eq!(result.len(), 13);
+        assert_eq!(
+            result,
+            &[55, 56, 57, 58, 59, 50, 51, 52, 63, 64, 65, 66, 67]
+        );
+    }
+
+    #[test]
+    fn one_step_split_middle_part_inside_rule() {
+        let seeds = (55, 68);
+        let group = vec![vec![50u32, 60, 2]];
+
+        let result = map_seed_range_in_group(seeds, &group);
+
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0], (81, 95));
+    }
 }
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use log::{debug, info};
+use num::Integer;
 
 const INPUT: &str = include_str!("../input.txt");
 const EXAMPLE: &str = include_str!("../example.txt");
@@ -7,16 +10,99 @@ fn main() {
     aoc::run_with_bench(INPUT, EXAMPLE, &|aoc| {
         let lines = aoc.read_input_lines();
 
-        (part1(&lines), part2(&lines))
+        (0, part2(&lines))
     });
 }
 
 fn part1(lines: &[&str]) -> u32 {
-    0
+    let directions = lines[0];
+    debug!("{directions:?}");
+    let mut dirs = directions.chars().cycle();
+    let nodes = lines[2..]
+        .iter()
+        .map(|line| (&line[0..=2], (&line[7..=9], &line[12..=14])))
+        .collect::<HashMap<_, _>>();
+    debug!("{nodes:?}");
+
+    let mut steps: u32 = 0;
+    let mut current = "AAA";
+    while current != "ZZZ" {
+        let next_dir = dirs.next();
+        debug!("{current} {next_dir:?}");
+        if let Some('L') = next_dir {
+            current = nodes[current].0
+        } else {
+            current = nodes[current].1
+        }
+
+        steps += 1;
+    }
+
+    return steps;
 }
 
-fn part2(_lines: &[&str]) -> u32 {
-    0
+fn part2(lines: &[&str]) -> u64 {
+    let directions = lines[0];
+    debug!("{directions:?}");
+    let mut dirs = directions.chars().cycle();
+    let nodes = lines[2..]
+        .iter()
+        .map(|line| (&line[0..=2], (&line[7..=9], &line[12..=14])))
+        .collect::<HashMap<_, _>>();
+    debug!("{nodes:?}");
+
+    let mut steps: u64 = 0;
+    let mut current = nodes
+        .keys()
+        .filter(|key| key.ends_with('A'))
+        .map(|a| a.to_owned())
+        .collect::<Vec<_>>();
+    debug!("Start values: {current:?}");
+
+    let cycles = current
+        .iter()
+        .map(|&start| {
+            let mut dirs = directions.chars().cycle();
+            let mut c = start;
+            let mut steps: u64 = 0;
+            while !c.ends_with('Z') {
+                let next_dir = dirs.next();
+                debug!("{c} {next_dir:?}");
+                if let Some('L') = next_dir {
+                    c = nodes[c].0
+                } else {
+                    c = nodes[c].1
+                }
+
+                steps += 1;
+            }
+            steps
+        })
+        .inspect(|c| info!("{c}"))
+        .collect::<Vec<u64>>();
+
+    info!("{cycles:?}");
+
+    let lcm = cycles.into_iter().reduce(|a, b| a.lcm(&b)).unwrap();
+    /*
+    while !current.iter().all(|c| c.ends_with('Z')) {
+                let next_dir = dirs.next();
+        if steps % 1000000 == 0 {debug!("{steps} {current:?} {next_dir:?}")}
+        //debug!("{current:?} {next_dir:?}");
+        if let Some('L') = next_dir {
+            current = current.iter().map(|&c| nodes[c].0).collect::<Vec<_>>();
+        } else {
+            current = current.iter().map(|&c| nodes[c].1).collect::<Vec<_>>();
+
+        }
+
+        steps +=1;
+    }
+
+
+    return steps
+     */
+    lcm
 }
 
 #[cfg(test)]
